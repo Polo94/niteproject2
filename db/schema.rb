33a788aecbc1e5e3708e422a4145f824a64f7881
Polo94 <paul.henry.claudant@gmail.com>
2018-10-01 07:01:29 +0200

    pricing & stripe views 1

--- db/schema.rb
+++ db/schema.rb
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_050908) do
     t.integer "card_exp_month"
     t.integer "card_exp_year"
     t.string "card_type"
+    t.boolean "admin"
     t.boolean "subscribed"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
